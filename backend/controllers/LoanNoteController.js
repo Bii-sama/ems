6402aad3659c06f62c4deb8da2b28b2d662e13a5
Abidemi <mail.bidemi75@gmail.com>
2024-02-06 15:53:53 +0100

    loanNote done done

--- backend/controllers/LoanNoteController.js
+++ backend/controllers/LoanNoteController.js
@@ -29,11 +29,35 @@ const createLoanNote = async (req, res) =>{
 
  } catch (error) {
     res.status(400).json({ error: error.message})
+ }  
+}
+
+
+const getAllLoanNotesByDate = async(req, res) => {
+    const { month, year } = req.params
+
+
+ const startDate = new Date(year, month - 1, 1);
+ const endDate = new Date(year, month, 0);
+
+ try {
+   const loanNotes = await LoanNote.find({
+       date: { $gte: startDate, $lte: endDate }
+     })
+
+     if(loanNotes.length < 1 ){
+       res.status(204).json({msg : "There are no loanNotes this period"})
+     }
+
+     res.status(200).json(loanNotes)
+ } catch (error) {
+   res.ststus(400).json({ error : error.message})
  }
- 
+
+}
 
 
-  
-
-  
+module.exports = {
+    createLoanNote,
+    getAllLoanNotesByDate
 }
